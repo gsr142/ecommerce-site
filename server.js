bc827407e4ca9ac2c7779518bdb8728cc3c178e3
gsr142 <gsr142@gmail.com>
2023-08-06 11:18:21 -0700

    sync models to db before server starts

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('sequelize');
+const { Category, Product, Tag, ProductTag } = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// sync sequelize models to the database
+async function syncBeforeStart () {
+  let category = Category.sync().then(()=> console.log('Category synced'));
+  let product = Product.sync().then(() => console.log('Product synced'));
+  let tag = Tag.sync().then(() => console.log('Tag synced'));
+  let productTag = ProductTag.sync().then(() => console.log('ProductTag synced'));
+
+  await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`)
+  });
+
+}
+syncBeforeStart()   
+
+// Turn on the server
+
